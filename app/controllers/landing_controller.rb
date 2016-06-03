e577e84f9be9514b42d040c9fd88abbb45ae5d1a
Ian Dickinson <i.j.dickinson@gmail.com>
2016-06-03 16:30:18 +0100

    Added Welsh language version of home page

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,4 +1,8 @@
 class LandingController < ApplicationController
   def index
+    template = "landing/index"
+    template = "#{template}-#{params[:lang]}" if params[:lang]
+
+    render template: template
   end
 end
