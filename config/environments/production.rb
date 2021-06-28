fab1e5c3e6cbb432be4aacf3bdba40883679e2fc
bogdan <marc.bogdan96@yahoo.com>
2021-06-28 11:53:12 +0100

    JSON Rails Logger gem uses another formatter, old one removed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
+  # config.log_formatter = ::Logger::Formatter.new
 
   config.logger = JsonRailsLogger::Logger.new($stdout)
 
