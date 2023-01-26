67980fec945b6eb00d39d7dae4d755f449e8ec3d
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2023-01-26 20:50:07 +0000

    build: updated version cadence
    Refactored the version cadence creation to include a SUFFIX value if provided; otherwise no SUFFIX is included in the version number.

--- app/lib/version.rb
+++ app/lib/version.rb
@@ -3,6 +3,7 @@
 module Version
   MAJOR = 1
   MINOR = 6
-  REVISION = 0
-  VERSION = "#{MAJOR}.#{MINOR}.#{REVISION}"
+  REVISION = 1
+  SUFFIX = nil
+  VERSION = "#{MAJOR}.#{MINOR}.#{REVISION}#{SUFFIX && ".#{SUFFIX}"}"
 end
