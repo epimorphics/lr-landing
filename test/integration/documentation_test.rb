1f73fa2460cb0994ab7fdc598d3f3f2fc7f56a9d
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2024-03-12 16:54:57 +0000

    test: updated ppd doc link tests
    first test verifies older link redirects permanently, second test verifies redirection also is permanent, third test ensures the link responds with success status (200), and fourth test ensures the page's opening content is what's expected

--- test/integration/documentation_test.rb
+++ test/integration/documentation_test.rb
@@ -14,8 +14,24 @@ class DocumentationTest < ActionDispatch::IntegrationTest
     assert_equal(302, response.status)
   end
 
-  test 'PPD docs page' do
+  test 'older PPD doc link redirects with permanent 301 status' do
     get '/doc/ppd'
+    assert_equal(301, response.status)
+  end
+
+  test 'old PPD doc link redirects with permanent 301 status' do
+    get '/app/root/doc/ppd'
+    assert_equal(301, response.status)
+  end
+
+  test 'ppd_doc_path variable links correctly' do
+    get ppd_doc_path
+    assert_response :success
+  end
+
+  test 'ppd doc loads correctly' do
+    get ppd_doc_path
     assert_select 'h1', 'Price Paid Linked Data'
+    assert_select 'h2', 'What does the Price Paid Dataset consist of?'
   end
 end
