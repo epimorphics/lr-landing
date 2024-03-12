cbd3cab7c8e630a13f928a9357e4e0add4b57759
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2024-03-12 16:43:08 +0000

    fix: resolves missing ppd doc issue
    also includes permanent redirections for outdated variances of the ppd doc path value

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
   get 'landing/hpi', to: 'landing#hpi'
   get 'doc/hpi', to: 'doc#hpi', as: 'hpi_doc'
   get 'doc/ukhpi', to: redirect('/app/ukhpi/doc', status: 302)
-  get 'doc/ppd', to: 'doc#ppd', as: 'ppd_doc'
+  get 'app/doc/ppd', to: 'doc#ppd', as: 'ppd_doc'
+  get 'doc/ppd', to: redirect('/app/doc/ppd', status: 301)
+  get 'app/root/doc/ppd', to: redirect('app/doc/ppd', status: 301)
   get 'doc/ukhpi-dsd', to: redirect('/app/ukhpi/doc/ukhpi-dsd', status: 302)
   get 'doc/ukhpi-user-guide', to: redirect('/app/ukhpi/doc/ukhpi-user-guide', status: 302)
   get 'doc/accessibility', to: 'doc#accessibility'
