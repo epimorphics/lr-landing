3e4342039132fc9493898b6115bf1e2346b8f3b8
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2025-02-26 15:44:16 +0000

    fix: Improve error handling in application controller
    - Added custom error handling for various exceptions.
    - Implemented specific methods to render 400, 403, 404, 422, and 500 error pages.
    - Enhanced internal error logging with Sentry integration.
    - Introduced a method to reset the response body before rendering errors.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,6 +27,93 @@ class ApplicationController < ActionController::Base
     expires_in 5.minutes, public: true, must_revalidate: true if Rails.env.production?
   end
 
+
+  # Handle specific types of exceptions and render the appropriate error page
+  # or attempt to render a generic error page if no specific error page exists
+  unless Rails.application.config.consider_all_requests_local
+    rescue_from StandardError do |e|
+      # Trigger the appropriate error handling method based on the exception
+      case e.class
+      when ActionController::RoutingError, ActionView::MissingTemplate
+        :render404
+      when ActionController::InvalidCrossOriginRequest
+        :render403
+      when ActionController::BadRequest, ActionController::ParameterMissing
+        :render400
+      else
+        :handle_internal_error
+      end
+    end
+  end
+
+  # Render the appropriate error page based on the exception
+  def handle_internal_error(exception)
+    case exception.instance_of?
+    when ArgumentError
+      render_error(400)
+    when UnprocessableEntity
+      render_error(422)
+    else
+      Rails.logger.warn "No explicit error page for exception #{exception} - #{exception.class}"
+      # Instrument ActiveSupport::Notifications for internal server errors only:
+      sentry_code = instrument_internal_error(exception)
+      render_error(500, sentry_code)
+    end
+  end
+
+  def render_400(_exception = nil) # rubocop:disable Naming/VariableNumber
+    render_error(400)
+  end
+
+  def render_403(_exception = nil) # rubocop:disable Naming/VariableNumber
+    render_error(403)
+  end
+
+  def render_404(_exception = nil) # rubocop:disable Naming/VariableNumber
+    render_error(404)
+  end
+
+  def render_500(_exception = nil) # rubocop:disable Naming/VariableNumber
+    render_error(500)
+  end
+
+  def render_error(status, sentry_code = nil)
+    reset_response
+
+    status = Rack::Utils::SYMBOL_TO_STATUS_CODE[status] if status.is_a?(Symbol)
+
+    respond_to do |format|
+      format.html { render_html_error_page(status, sentry_code) }
+      # Anything else returns the status as human readable plain string
+      format.all { render plain: Rack::Utils::HTTP_STATUS_CODES[status].to_s, status: status }
+    end
+  end
+
+  def render_html_error_page(status, sentry_code)
+    render 'exceptions/error_page',
+           locals: { status: status, sentry_code: sentry_code },
+           layout: true,
+           status: status
+  end
+
+  def render_request_error(user_selections, status_code)
+    # Convert status code to integer if it is a symbol
+    status_code = Rack::Utils::SYMBOL_TO_STATUS_CODE[status_code] if status_code.is_a?(Symbol)
+    respond_to do |format|
+      format.html { render_html_error_page(status_code, nil) }
+
+      format.json do
+        render(json: { status: 'request error' }, status: status_code)
+      end
+    end
+  end
+
+  def reset_response
+    self.response_body = nil
+  end
+
+
+
   # Notify subscriber(s) of an internal error event with the payload of the
   # exception once done
   # @param [exc] exp the exception that caused the error
@@ -38,6 +125,7 @@ class ApplicationController < ActionController::Base
       message: exc&.message || exc,
       status: exc&.status || Rack::Utils::SYMBOL_TO_STATUS_CODE[exc]
     }
+    byebug
     err[:type] = exc.class&.name if exc&.class
     err[:cause] = exc&.cause if exc&.cause
     err[:backtrace] = exc&.backtrace if exc&.backtrace && Rails.env.development?
