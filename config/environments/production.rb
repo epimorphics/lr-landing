f8e41b2154bbfe1d6bbb712cf7357874c9774807
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2025-04-02 16:45:46 +0100

    Build: Replaces uglifier with terser
    Replaces the JavaScript compressor to use terser.
    
    - Improves build performance by switching to a more efficient compressor.
    - Comments out the previous compressor.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,8 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV.fetch('RAILS_SERVE_STATIC_FILES', true)
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = :terser
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
