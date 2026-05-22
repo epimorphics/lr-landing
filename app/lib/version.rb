823a356ab945a4fb2eb7255560262fa9ac41f877
joescottdave <joseph.davies@outlook.com>
2026-05-22 16:01:59 +0100

    ci: add missing  file for running e2e tests in ci

--- app/lib/version.rb
+++ app/lib/version.rb
@@ -3,7 +3,7 @@
 module Version
   MAJOR = 2
   MINOR = 3
-  PATCH = 3
-  SUFFIX = ''
+  PATCH = 4
+  SUFFIX = 'prerelease'
   VERSION = "#{MAJOR}.#{MINOR}.#{PATCH}#{SUFFIX && ".#{SUFFIX}"}".freeze
 end
